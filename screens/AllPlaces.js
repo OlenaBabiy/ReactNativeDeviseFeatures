460b320f023c0864432c69203192410bb3ae542e
OlenaBabii <olenkababiy04@gmail.com>
2024-12-06 15:41:34 +0200

    added changes

---
+++ screens/AllPlaces.js
@@ -0,0 +1,10 @@
+import { View, StyleSheet } from "react-native";
+import PlacesList from "../components/Places/PlacesList";
+
+function AllPlaces() {
+  return <PlacesList />;
+}
+
+export default AllPlaces;
+
+const styles = StyleSheet.create({});
